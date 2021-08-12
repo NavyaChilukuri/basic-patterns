1ce945f4b4262af264962e7c41d2c22845902168
NavyaChilukuri <88478504+NavyaChilukuri@users.noreply.github.com>
2021-08-12 00:00:55 -0700

    Add files via upload

---
+++ Firstclass.java
@@ -0,0 +1,17 @@
+
+public class Firstclass{
+	public static void main(String args[])
+	{
+		for(char i=65;i<91;i++)
+		{
+			for(char j=65;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			   
+		
+		System.out.print("\n");
+		
+		}
+	}
+}
